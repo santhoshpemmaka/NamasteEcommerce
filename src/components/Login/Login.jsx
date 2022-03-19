0e6992830222222ad5bdfc1b15b14ff500a61b82
Santhosh_Pemmaka <santhosh.pemmaka@gmail.com>
2022-03-19 12:39:38 +0530

    adding: showing password icon in the login and signup Page.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import "./Login.scss";
 const Login = () => {
+	const [loginDetails, setloginDetails] = useState({
+		username: "",
+		password: "",
+		shownPassword: false,
+	});
+	const btnHandler = () => {
+		setloginDetails({username: "", password: ""});
+	};
+	const iconHandler = () => {
+		setloginDetails({
+			...loginDetails,
+			shownPassword: !loginDetails.shownPassword,
+		});
+	};
 	return (
 		<div className='login-container'>
 			<div className='login-component'>
@@ -9,18 +23,38 @@ const Login = () => {
 				<div className='login-inputs'>
 					<input
 						className='login-input'
+						value={loginDetails.username}
 						type='email'
 						placeholder='Enter your email here'
+						onChange={(e) =>
+							setloginDetails({...loginDetails, username: e.target.value})
+						}
 					/>
-					<input
-						className='login-input'
-						type='password'
-						placeholder='Enter your password here'
-					/>
+					<div className='login-password'>
+						<input
+							className='login-input'
+							value={loginDetails.password}
+							type={loginDetails.shownPassword ? "text" : "password"}
+							placeholder='Enter your password here'
+							onChange={(e) =>
+								setloginDetails({...loginDetails, password: e.target.value})
+							}
+						/>
+						{loginDetails.shownPassword ? (
+							<i
+								onClick={iconHandler}
+								className='fas fa-eye-slash password-icon'></i>
+						) : (
+							<i onClick={iconHandler} className='fas fa-eye password-icon'></i>
+						)}
+					</div>
+
 					<Link className='forgot-alink' to='/'>
 						<label className='forgot-password'>Forgot your password?</label>
 					</Link>
-					<button className='login-input login-btn'>LOGIN</button>
+					<button className='login-input login-btn' onClick={btnHandler}>
+						LOGIN
+					</button>
 					<label className='login-text'>
 						Not a user yet ? <Link to='/signup'>Create your account</Link>
 					</label>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,65 +1,139 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import "./SignUp.scss";
 
 const SignUp = () => {
+	const [signupDetails, setsignupDetails] = useState({
+		firstName: "",
+		lastName: "",
+		emailName: "",
+		passWord: "",
+		confirmPassword: "",
+		termsConditions: false,
+		shownPassword: false,
+	});
+
+	const signupHandler = () => {
+		setsignupDetails({
+			firstName: "",
+			lastName: "",
+			emailName: "",
+			passWord: "",
+			confirmPassword: "",
+			termsConditions: false,
+			shownPassword: false,
+		});
+	};
+
+	const iconHandler = () => {
+		setsignupDetails({
+			...signupDetails,
+			shownPassword: !signupDetails.shownPassword,
+		});
+	};
 	return (
 		<div className='signup-container'>
 			<div className='spacer-3rem'></div>
-			<div class='signup-component'>
-				<label class='signup-header'>SIGN UP</label>
-				<div class='form-signup'>
-					<div class='componet-signup'>
-						<label class='label-signup-name'>First Name </label>
+			<div className='signup-component'>
+				<label className='signup-header'>SIGN UP</label>
+				<div className='form-signup'>
+					<div className='componet-signup'>
+						<label className='label-signup-name'>First Name </label>
 						<input
 							type='text'
-							class='signup-input'
+							value={signupDetails.firstName}
+							className='signup-input'
 							placeholder='Enter your first name'
+							onChange={(e) =>
+								setsignupDetails({...signupDetails, firstName: e.target.value})
+							}
 							required
 						/>
 					</div>
-					<div class='componet-signup'>
-						<label class='label-signup-name'>Last Name </label>
+					<div className='componet-signup'>
+						<label className='label-signup-name'>Last Name </label>
 						<input
 							type='text'
-							class='signup-input'
+							value={signupDetails.lastName}
+							className='signup-input'
 							placeholder='Enter your last name'
+							onChange={(e) =>
+								setsignupDetails({...signupDetails, lastName: e.target.value})
+							}
 							required
 						/>
 					</div>
-					<div class='componet-signup'>
-						<label class='label-signup-name'>Email </label>
+					<div className='componet-signup'>
+						<label className='label-signup-name'>Email </label>
 						<input
 							type='email'
-							class='signup-input'
+							value={signupDetails.emailName}
+							className='signup-input'
 							placeholder='sample@gmail.com'
+							onChange={(e) =>
+								setsignupDetails({...signupDetails, emailName: e.target.value})
+							}
 							required
 						/>
 					</div>
-					<div class='componet-signup'>
-						<label class='label-signup-name'>Password </label>
+					<div className='componet-signup'>
+						<label className='label-signup-name'>Password </label>
 						<input
 							type='password'
-							class='signup-input'
+							value={signupDetails.passWord}
+							className='signup-input'
 							placeholder='Enter new password'
+							onChange={(e) =>
+								setsignupDetails({...signupDetails, passWord: e.target.value})
+							}
 							required
 						/>
 					</div>
-					<div class='componet-signup'>
-						<label class='label-signup-name'>Confirm Password </label>
+					<div className='componet-signup'>
+						<label className='label-signup-name'>Confirm Password </label>
+						<div className='show-password'>
+							<input
+								type={signupDetails.shownPassword ? "text" : "password"}
+								value={signupDetails.confirmPassword}
+								className='signup-input'
+								placeholder='Re-type your password'
+								onChange={(e) =>
+									setsignupDetails({
+										...signupDetails,
+										confirmPassword: e.target.value,
+									})
+								}
+								required
+							/>
+							{signupDetails.shownPassword ? (
+								<i
+									onClick={iconHandler}
+									className='fas fa-eye-slash password-icon'></i>
+							) : (
+								<i
+									onClick={iconHandler}
+									className='fas fa-eye password-icon'></i>
+							)}
+						</div>
+					</div>
+					<div className='terms-conditons'>
 						<input
-							type='password'
-							class='signup-input'
-							placeholder='Re-type your password'
-							required
-						/>
-					</div>
-					<div class='terms-conditons'>
-						<input class='terms-checkbox' type='checkbox' />{" "}
+							className='terms-checkbox'
+							type='checkbox'
+							checked={signupDetails.termsConditions}
+							onChange={() =>
+								setsignupDetails({
+									...signupDetails,
+									termsConditions: !signupDetails.termsConditions,
+								})
+							}
+						/>{" "}
 						<label>I accept all terms & conditions.</label>
 					</div>
-					<button class='register-btn'>REGISTER</button>
-					<label class='sigup-register'>
+					<button className='register-btn' onClick={() => signupHandler()}>
+						REGISTER
+					</button>
+					<label className='sigup-register'>
 						Already registered? <Link to='/login'>Login here</Link>
 					</label>
 				</div>
