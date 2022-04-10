a761afee57dc98734e669959059f9d0006223ed0
Santhosh_Pemmaka <santhosh.pemmaka@gmail.com>
2022-04-11 00:09:37 +0530

    adding: images adding loacally

--- src/database.js
+++ src/database.js
@@ -1,3 +1,12 @@
+import {
+	product1,
+	product2,
+	product3,
+	product4,
+	product5,
+	product6,
+} from "./Assests";
+
 export const brandNames = [
 	{
 		name: "Domyos",
@@ -34,39 +43,35 @@ export const sliderImages = [
 	{
 		title: "First Slider Image",
 		description: "This is the first image",
-		urls: "https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fs913279%2Fk%240a4673bd73433da6ce81b34429a752ab%2Fhp%2520na.jpg%3Ff%3D1920x500%26format%3Dauto&w=1920&q=75",
+		urls: product2,
 	},
 	{
 		title: "second Slider Image",
 		description: "This is the first image",
-		urls: "https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fs913586%2Fk%24d8bbb7d546f89df102c34657875b46be%2Fpolo%2520desktop%25201.jpg%3Ff%3D1920x500%26format%3Dauto&w=1920&q=75",
+		urls: product1,
 	},
 	{
 		title: "third Slider Image",
 		description: "This is the first image",
-		urls: "https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fs913279%2Fk%240a4673bd73433da6ce81b34429a752ab%2Fhp%2520na.jpg%3Ff%3D1920x500%26format%3Dauto&w=1920&q=75",
+		urls: product2,
 	},
 	{
 		title: "Fourth Slider Image",
 		description: "This is the first image",
-		urls: "https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fs913586%2Fk%24d8bbb7d546f89df102c34657875b46be%2Fpolo%2520desktop%25201.jpg%3Ff%3D1920x500%26format%3Dauto&w=1920&q=75",
+		urls: product1,
 	},
 ];
 export const productImages = [
 	{
-		imageUrl:
-			"https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fp424563%2F65eaabd17d874331ce1d119bc109c464%2Fp424563.jpg%3Fformat%3Dauto&w=384&q=75",
+		imageUrl: product3,
 	},
 	{
-		imageUrl:
-			"https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fp1123657%2Fk%2445c38f2355993a341936413bef0be4e6%2Fmen-s-photographer-jacket-500-brown.jpg%3F%26f%3D250x250&w=384&q=75",
+		imageUrl: product4,
 	},
 	{
-		imageUrl:
-			"https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fp1420005%2F4067736fa30c86fd7c12291607df5399%2Fp1420005.jpg%3Fformat%3Dauto&w=384&q=75",
+		imageUrl: product5,
 	},
 	{
-		imageUrl:
-			"https://www.decathlon.in/_next/image?url=https%3A%2F%2Fcontents.mediadecathlon.com%2Fp2073324%2F79be1a70bf19be863141ef791603516e%2Fp2073324.jpg%3Fformat%3Dauto&w=384&q=75",
+		imageUrl: product6,
 	},
 ];
